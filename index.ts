83f61fa144aa3d239e0065c14824e90a6c16e3b9
Aitor C. Figueroa <aitorcf@gmail.com>
2022-07-13 07:23:12 +0200

    Config HelloRouter, HelloController & server

--- index.ts
+++ index.ts
@@ -1,29 +1,18 @@
-import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import server from '@/server'
+import { LogSuccess, LogError } from '@/utils/logger'
 
-// Configuration the .env file
+// Configuration .env file
 dotenv.config()
 
-// Create Express App
-const app: Express = express()
 const port: string | number = process.env.PORT || 8000
 
-// Create an App Route
-app.get('/', function (req: Request, res: Response) {
-  res.status(200).send({
-    message: 'Goodbye, world'
-  })
+// Execute SERVER and Listen Requests to PORT
+server.listen(port, () => {
+  LogSuccess(`[SERVER ON]: Running in http://localhost:${port}/api`)
 })
 
-app.get('/hello', function (req: Request, res: Response) {
-  const name = req.query.name
-
-  res.status(200).send({
-    message: `Hola, ${name || 'anónimo'}`
-  })
-})
-
-// Execute APP and Listen Requests to PORT
-app.listen(port, () => {
-  console.log(`EXPRESS SERVER: Running at http://localhost:${port}`)
+// Control SERVER ERROR
+server.on('error', (error) => {
+  LogError(`[SERVER ERROR]: ${error}`)
 })
